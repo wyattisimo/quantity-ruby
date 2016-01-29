ac3f35556bb3f8c782a1bf2da9bb338908bf6021
Jared Wyatt <j@wyatt.co>
2016-01-29 02:45:03 -0500

    add power operator

--- lib/quantity/arithmetic.rb
+++ lib/quantity/arithmetic.rb
@@ -25,6 +25,10 @@ class Quantity < Numeric
       value / coerce_other(other)
     end
 
+    def **(other)
+      value ** coerce_other(other)
+    end
+
     private def coerce_other(other)
       other = other.is_a?(Quantity) ? other.value : other
       raise TypeError.new("#{other.class} can't be coerced into a #{self.class}") unless other.is_a?(Numeric)
